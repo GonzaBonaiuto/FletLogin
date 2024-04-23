ff03fd96c3b309fb8c2e7bd5c2ecc1c3d3fb1bf9
Gonza Bonaiuto <gonzalo.bonaiuto@gmail.com>
2024-04-22 22:42:45 -0300

    Commit primer dia de trabajo en login con python

--- Project/Login/main.py
+++ Project/Login/main.py
@@ -1,10 +1,80 @@
 import flet as ft
 
 
-container = ft.Container
+container = ft.Container(
+    ft.Column([
+        ft.Container(
+            ft.Text(
+                "Iniciar Sesión",
+                width = 320,
+                size = 30,
+                text_align = "center",
+                weight = "w900"   
+            ),
+            padding = ft.padding.only(20,20) 
+        ),
+        ft.Container(
+            ft.TextField(
+                width = 280,
+                height = 40,
+                hint_text = "Correo electrónico",
+                border = "underline",
+                color = "black",
+                prefix_icon = ft.icons.EMAIL
+            ),
+            padding = ft.padding.only(20,20)
+        ),
+                ft.Container(
+            ft.TextField(
+                width = 280,
+                height = 40,
+                hint_text = "Contraseña",
+                border = "underline",
+                color = "black",
+                prefix_icon = ft.icons.LOCK
+            ),
+            padding = ft.padding.only(20,20)
+        ),
+        ft.Container(
+            ft.Checkbox(
+                label = "Recordar contraseña",
+                check_color = "black"
+            ),
+            padding = ft.padding.only(80)
+        ),
+        ft.Container(
+            ft.ElevatedButton(
+                text = "INICIAR",
+                width = 280,
+                bgcolor = "black"
+            ),
+            padding = ft.padding.only(20,20)
+        ),
+        ft.Text(
+            "Iniciar sesión con",
+            text_align = "center",
+            width = 320
+            ),
+        
+    ],
+    alignment = ft.MainAxisAlignment.SPACE_EVENLY
+    ),
+    border_radius = 20,
+    width = 320,
+    height =500,
+    gradient = ft.LinearGradient([
+        ft.colors.PURPLE,
+        ft.colors.PINK,
+        ft.colors.RED     
+    ])
+)
+
 
 
 def main (page: ft.page):
-    pass
+    page.bgcolor = "black"
+    page.vertical_alignment = "center"
+    page.horizontal_alignment = "center"
+    page.add(container)
 
 ft.app(target=main)
